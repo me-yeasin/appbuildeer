a2d09cdc482d56f0cb7a4db40d76671fa2244c0d
me-yeasin <md-yeasinarafat@hotmail.com>
2024-05-23 15:29:27 +0600

    our offer section created

---
+++ src/app/services/design/[id]/page.tsx
@@ -0,0 +1,7 @@
+import ServiceDesignScreen from "@/screens/service/design_screen/service_design_screen";
+
+const ServiceDesignPage = ({}: { params: { id: string } }) => {
+  return <ServiceDesignScreen />;
+};
+
+export default ServiceDesignPage;
---
+++ src/screens/service/design_screen/service_design_screen.tsx
@@ -0,0 +1,98 @@
+"use client";
+
+import Image from "next/image";
+
+import BodyText from "@/common_components/text/body_text/body_text";
+import TitleText from "@/common_components/text/title_text/title_text";
+import classes from "./service_design_screen.module.scss";
+
+interface ServiceDesignScreenProps {}
+
+const ServiceDesignScreen: React.FC<ServiceDesignScreenProps> = (props) => {
+  return (
+    <>
+      <section className={classes["service-design__header-section"]}>
+        <div className={classes["service-design__header-section-container"]}>
+          <Image
+            src={"/images/local/test_img.png"}
+            alt="service design screen header"
+            height={400}
+            width={400}
+            priority
+            className={classes["img"]}
+          />
+          <h1 className={classes["service-design__header-section-title"]}>
+            Icons
+          </h1>
+        </div>
+      </section>
+      <section className={classes["service-design__why-section"]}>
+        <TitleText>Why Custom Icons?</TitleText>
+        <div className={classes["why-section__img-container"]}>
+          <Image
+            src={"/images/local/custom_icon_img.png"}
+            alt="custom icon photo"
+            height={300}
+            width={300}
+            className={classes["img-container__img"]}
+          />
+        </div>
+        <BodyText className={classes["why-section__description"]}>
+          Lorem ipsum dolor sit amet consectetur. Cursus amet viverra id
+          placerat. Elit luctus dictum massa bibendum neque porta sit id. A eu
+          sit turpis nunc. Feugiat ut at neque phasellus tristique. Orci netus
+          sagittis rhoncus elit. Felis elementum adipiscing gravida potenti.
+          Sapien ultrices fermentum interdum aliquet gravida nulla enim. Nullam
+          morbi amet habitasse.
+        </BodyText>
+      </section>
+      <section className={classes["service-design__offer-section"]}>
+        <TitleText>Our offer with icons</TitleText>
+        <ul className={classes["offer-section__offer-list"]}>
+          <li className={classes["offer-list__item"]}>
+            <Image
+              src={"/images/local/offer_custom_icon_img.png"}
+              alt="custom icon image"
+              height={100}
+              width={100}
+              className={classes["item__img"]}
+            />
+            <p className={classes["item__name"]}>Icon/Icon Set</p>
+          </li>
+          <li className={classes["offer-list__item"]}>
+            <Image
+              src={"/images/local/offer_custom_icon_img.png"}
+              alt="custom icon image"
+              height={100}
+              width={100}
+              className={classes["item__img"]}
+            />
+            <p className={classes["item__name"]}>Icon/Icon Set</p>
+          </li>
+          <li className={classes["offer-list__item"]}>
+            <Image
+              src={"/images/local/offer_custom_icon_img.png"}
+              alt="custom icon image"
+              height={100}
+              width={100}
+              className={classes["item__img"]}
+            />
+            <p className={classes["item__name"]}>Icon/Icon Set</p>
+          </li>
+          <li className={classes["offer-list__item"]}>
+            <Image
+              src={"/images/local/offer_custom_icon_img.png"}
+              alt="custom icon image"
+              height={100}
+              width={100}
+              className={classes["item__img"]}
+            />
+            <p className={classes["item__name"]}>Icon/Icon Set</p>
+          </li>
+        </ul>
+      </section>
+    </>
+  );
+};
+
+export default ServiceDesignScreen;
